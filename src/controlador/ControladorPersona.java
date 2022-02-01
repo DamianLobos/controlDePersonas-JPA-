193a7446ad01ffb5b3aaa81f12cd417180314708
Damian Lobos <damianlobos24@gmail.com>
2022-02-01 19:53:25 -0300

    Metodos para ingresar, modificar y eliminar datos de la base de datos, ademas de obtener una lista con los elementos de la misma

--- src/controlador/ControladorPersona.java
+++ src/controlador/ControladorPersona.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controlador;
 
-/**
- *
- * @author Usuario
- */
+import conexion.Conexion;
+import entidades.Persona;
+import javax.persistence.EntityManager;
+
 public class ControladorPersona {
+    private EntityManager entityManager(){
+        return Conexion.getConex().getFactory().createEntityManager();
+    }
     
+    public void crearPersona(Persona p1){
+        EntityManager em = entityManager();
+        try{
+            em.getTransaction().begin();
+            em.persist(p1);
+            em.getTransaction().commit();
+        }catch(Exception ex){
+            System.out.println("La transaccion no pudo realizarse");
+            em.getTransaction().rollback();
+        }
+    }
+    
+    public void eliminarPersona(Persona p1){
+        EntityManager em = entityManager();
+        try{
+            em.getTransaction().begin();
+            em.remove(em.merge(p1));
+            em.getTransaction().commit();
+        }catch(Exception ex){
+            System.out.println("No pudo eliminarse la persona");
+            em.getTransaction().rollback();
+        }
+    }
 }
